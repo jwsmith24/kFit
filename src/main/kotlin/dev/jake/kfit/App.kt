bbe66ab9fec7331fa3f44fff7e4a805223c337d3
jake <jsmith181997@gmail.com>
2026-03-07 10:45:27 -0600

    add serialized data class for user, add find by id
    Signed-off-by: jake <jsmith181997@gmail.com>

--- src/main/kotlin/dev/jake/kfit/App.kt
+++ src/main/kotlin/dev/jake/kfit/App.kt
@@ -1,13 +1,12 @@
 package dev.jake.kfit
 
-import dev.jake.generated.tables.Users
-import dev.jake.generated.tables.Users.USERS
 import dev.jake.kfit.di.DaggerAppComponent
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.flywaydb.core.Flyway
@@ -35,10 +34,22 @@ fun Application.module() {
 
     routing {
         get("/") {call.respondText("Welcome to kFit")}
+
         get("/health") {
             call.respond(appComponent.metricsService().healthCheck())
         }
+
         get("/users") { call.respond(userRepository.findAll()) }
+
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+                ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+            val user = userRepository.findById(id)
+                ?: return@get call.respond(HttpStatusCode.NotFound)
+
+            call.respond(user)
+        }
 
     }
 }
--- src/main/kotlin/dev/jake/kfit/repos/UserRepository.kt
+++ src/main/kotlin/dev/jake/kfit/repos/UserRepository.kt
@@ -1,16 +1,26 @@
 package dev.jake.kfit.repos
 
 import dev.jake.generated.tables.Users.USERS
+import kotlinx.serialization.Serializable
 import org.jooq.DSLContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@Serializable
+data class User(val id: Int, val name: String)
+
 @Singleton
 class UserRepository @Inject constructor(private val dsl: DSLContext) {
 
-    fun findAll(): List<String> =
+    fun findAll(): List<User> =
         dsl.select(USERS.ID, USERS.NAME)
             .from(USERS)
             // can pass the transform function directly into fetch as lambda
-            .fetch { "${it[USERS.ID]}: ${it[USERS.NAME]}" }
+            .fetch { User(it[USERS.ID], it[USERS.NAME]) }
+
+
+    fun findById(id: Int): User? =
+        dsl.selectFrom(USERS)
+            .where(USERS.ID.eq(id))
+            .fetchOne { User(it[USERS.ID], it[USERS.NAME])}
 }
